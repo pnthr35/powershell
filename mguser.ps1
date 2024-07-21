e3263d3044d4e4789266704e07a21939ae8d3717
pnthr35 <pnthr35@gmail.com>
2024-07-20 22:08:15 -0700

    Adjust MgUser

--- mguser.ps1
+++ mguser.ps1
@@ -1,1 +1,7 @@
 # Testing User interactions in Graph Powershell
+$UserLookup = @{}
+[array]$Users = Get-MgUser -All -Filter "usertype eq 'Member'" -Property Id, DisplayName
+
+foreach ($User in $Users) {
+    $UserLookup.Add($User.Id, $User.DisplayName)
+}
