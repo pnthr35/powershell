9cd4d6e7cbb10cd9a1eb739c17b813813d67eb07
pnthr35 <pnthr35@gmail.com>
2024-07-20 22:58:29 -0700

    Adjust AuthN

--- mguser.ps1
+++ mguser.ps1
@@ -1,7 +1,11 @@
 # Testing User interactions in Graph Powershell
+Connect-MgGraph -Scopes "Directory.ReadWrite.All","User.ReadWrite.All"
 $UserLookup = @{}
 [array]$Users = Get-MgUser -All -Filter "usertype eq 'Member'" -Property Id, DisplayName
 
 foreach ($User in $Users) {
     $UserLookup.Add($User.Id, $User.DisplayName)
 }
+
+Write-Host $UserLookup
+Write-Host "Done!"
