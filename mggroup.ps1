8bff284e107dccd0dee4fbea5240663df9b3e3c5
pnthr35 <pnthr35@gmail.com>
2024-08-14 23:55:22 -0700

    Adjust MgGroup

--- mggroup.ps1
+++ mggroup.ps1
@@ -1,2 +1,5 @@
 # Testing Group interactions with Graph Powershell
 Connect-MgGraph -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
+
+$Groups = Get-MgGroup
+Write-Host $Groups
