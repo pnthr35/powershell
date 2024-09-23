ed5e8d579f518ea112e2722f6d659892a0ef0cfa
pnthr35 <pnthr35@gmail.com>
2024-09-23 01:32:40 -0700

    Adjust mguser

--- mguser.ps1
+++ mguser.ps1
@@ -3,10 +3,12 @@ Connect-MgGraph -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -
 $UserLookup = @{}
 [array]$Users = Get-MgUser -All -Filter "usertype eq 'Member'" -Property Id, DisplayName
 
+# Get Users
 foreach ($User in $Users) {
     $UserLookup.Add($User.Id, $User.DisplayName)
 }
 
+# Output Users
 Write-Host $UserLookup
 Write-Host "All Users:"
 foreach ($User in $Users) {
