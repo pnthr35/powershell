4d5542573a659961cfaf5ce2cdf8d69073e2be98
pnthr35 <pnthr35@gmail.com>
2024-10-04 00:35:12 -0700

    adjust mgintune

--- mgintune.ps1
+++ mgintune.ps1
@@ -1,2 +1,9 @@
 # Interaction with Intune
-Connect-MgGraph -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
+Connect-MgGraph 
+
+# -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
+
+$devices = Get-MgDeviceManagementManagedDevice
+$devices | ForEach-Object {
+    Write-Output $_.DisplayName
+}
