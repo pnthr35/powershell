3fa19a0c220cc3a40eac7a7c0577806ff89c42cb
pnthr35 <pnthr35@gmail.com>
2024-10-19 13:36:00 -0700

    adjust mglicense

--- mglicense.ps1
+++ mglicense.ps1
@@ -1,8 +1,17 @@
-Connect-MgGraph
+Connect-MgGraph -Scopes "Directory.ReadWrite.All","User.ReadWrite.All", "Group.ReadWrite.All"
 
 $Users = Get-MgUser
+$PowerAutomateLicense = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'FLOW_FREE'
 
 foreach ($item in $Users) {
    Write-Host $item.DisplayName
+
+   # Assign licenses to Power Automate Free
+   Update-MgUser -UserId $item.Id -UsageLocation US
+   Set-MgUserLicense -UserId $item.Id -AddLicenses @{SkuId = $PowerAutomateLicense.SkuId} -RemoveLicenses @()
+
+   # Remove licenses for Power Automate Free???
+   # Set-MgUserLicense -UserId $item.Id -RemoveLicenses @("FLOW_FREE") -AddLicenses @{}
+
    Get-MgUserLicenseDetail -UserId $item.Id | Format-List
 }
