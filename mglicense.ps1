27df8bcb07bfc49662c2722640eb9e38b85e1a8c
pnthr35 <pnthr35@gmail.com>
2024-10-19 13:40:41 -0700

    adjust mglicense

--- mglicense.ps1
+++ mglicense.ps1
@@ -1,17 +1,17 @@
-Connect-MgGraph -Scopes "Directory.ReadWrite.All","User.ReadWrite.All", "Group.ReadWrite.All"
+Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.ReadWrite.All"
 
 $Users = Get-MgUser
 $PowerAutomateLicense = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'FLOW_FREE'
 
 foreach ($item in $Users) {
-   Write-Host $item.DisplayName
+    Write-Host $item.DisplayName
 
-   # Assign licenses to Power Automate Free
-   Update-MgUser -UserId $item.Id -UsageLocation US
-   Set-MgUserLicense -UserId $item.Id -AddLicenses @{SkuId = $PowerAutomateLicense.SkuId} -RemoveLicenses @()
+    # Assign licenses to Power Automate Free
+    Update-MgUser -UserId $item.Id -UsageLocation US
+    Set-MgUserLicense -UserId $item.Id -AddLicenses @{SkuId = $PowerAutomateLicense.SkuId } -RemoveLicenses @()
 
-   # Remove licenses for Power Automate Free???
-   # Set-MgUserLicense -UserId $item.Id -RemoveLicenses @($PowerAutomateLicense.SkuId) -AddLicenses @{}
+    # Remove licenses for Power Automate Free???
+    # Set-MgUserLicense -UserId $item.Id -RemoveLicenses @($PowerAutomateLicense.SkuId) -AddLicenses @{}
 
-   Get-MgUserLicenseDetail -UserId $item.Id | Format-List
+    Get-MgUserLicenseDetail -UserId $item.Id | Format-List
 }
