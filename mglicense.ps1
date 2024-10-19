da166168e5c86a2c5fe5eb530144816f9c4a411f
pnthr35 <pnthr35@gmail.com>
2024-10-19 13:58:18 -0700

    adjust mglicense

--- mglicense.ps1
+++ mglicense.ps1
@@ -6,12 +6,18 @@ $PowerAutomateLicense = Get-MgSubscribedSku -All | Where-Object SkuPartNumber -e
 # Remember that User in Restricted AU's will not be affected, must use the AU Admin for this script
 foreach ($item in $Users) {
     Write-Host $item.DisplayName
+    
+    if ($item.DisplayName -eq "On-Premises Directory Synchronization Service Account")
+    {
+        Write-Host "Skipping AD Sync Account"
+        continue
+    }
 
-    # Assign licenses to Power Automate Free
-    Update-MgUser -UserId $item.Id -UsageLocation US
-    Set-MgUserLicense -UserId $item.Id -AddLicenses @{SkuId = $PowerAutomateLicense.SkuId } -RemoveLicenses @()
+    # Assign licenses to Power Automate Free, set Usage Location if needed first as well
+    # Update-MgUser -UserId $item.Id -UsageLocation US
+    # Set-MgUserLicense -UserId $item.Id -AddLicenses @{SkuId = $PowerAutomateLicense.SkuId } -RemoveLicenses @()
 
-    # Remove licenses for Power Automate Free???
+    # Remove licenses for Power Automate Free
     # Set-MgUserLicense -UserId $item.Id -RemoveLicenses @($PowerAutomateLicense.SkuId) -AddLicenses @{}
 
     Get-MgUserLicenseDetail -UserId $item.Id | Format-List
