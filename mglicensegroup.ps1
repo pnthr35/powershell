34fff3519b5989a103cf729ab49506af533b50be
pnthr35 <pnthr35@gmail.com>
2024-12-01 00:53:22 -0700

    Adjust group license script

--- mglicensegroup.ps1
+++ mglicensegroup.ps1
@@ -1,14 +1,18 @@
 Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.ReadWrite.All"
 
-$Groups = Get-MgGroup
+$groups = Get-MgGroup
 $PowerAutomateLicense = Get-MgSubscribedSku -All | Where-Object SkuPartNumber -eq 'FLOW_FREE'
 
+# Check if Group exists
 foreach ($item in $Groups)
 {
     if ($item.DisplayName -eq "Testing Group")
     {
         Write-Host "Group already exists..."
+        Write-Host $item.DisplayName
     }
+
 }
 
+# Set-MgGroupLicense -GroupId $groupId -AddLicenses $PowerAutomateLicense
 Write-Host "Done!"
