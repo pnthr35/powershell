62efef599c33b4a1ccc4811dd461b45a3ec47efe
pnthr35 <pnthr35@gmail.com>
2024-12-01 01:08:56 -0700

    Prep mggroup script

--- mggroup.ps1
+++ mggroup.ps1
@@ -1,5 +1,12 @@
 # Testing Group interactions with Graph Powershell
-Connect-MgGraph -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
+# Connect-MgGraph -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
+Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.ReadWrite.All"
 
 $Groups = Get-MgGroup
 Write-Host $Groups
+
+# See if a Group exists
+
+# If not, create the Group
+
+# Write all Groups to a .csv file
