a16bac4a21ea59499abb10262a23738e1fba889a
pnthr35 <pnthr35@gmail.com>
2024-12-05 22:57:52 -0700

    Fix group scripts

--- mggroup.ps1
+++ mggroup.ps1
@@ -5,19 +5,22 @@ Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.
 $Groups = Get-MgGroup
 
 # See if a Group exists
-foreach ($group in $groups)
-{
+foreach ($group in $groups) {
+    Write-Host $group
     Write-Host $group.DisplayName
-    if ($group.DisplayName -eq "GroupTestGraph")
-    {
-        Write-Host "Group exists"
+    if ($group.DisplayName -eq "GroupTestGraph") {
         $groupExists = $true
     }
 }
 
-if (!$groupExists)
-{
+if (!$groupExists) {
     New-MgGroup -DisplayName 'GroupTestGraph' -MailEnabled:$False -MailNickName 'grouptestgraph' -SecurityEnabled   
+}
+else {
+    Write-Host "reached else"
+    $group = Get-MgGroup -Filter "DisplayName eq 'GroupTestGraph'"
+    Write-Host $group.DisplayName
+    #Remove-MgGroup -GroupId $group.Id
 }
 
 # Write all Groups to a .csv file
