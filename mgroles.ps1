93ffa5fc18c8628929f60747e2c143044cc91a86
pnthr35 <pnthr35@gmail.com>
2025-01-08 17:32:56 -0700

    Add Roles script

---
+++ mgroles.ps1
@@ -0,0 +1,12 @@
+# Role information
+Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.ReadWrite.All"
+
+[array]$Roles = Get-MgDirectoryRole | Sort-Object displayName
+
+ForEach ($Role in $Roles) {
+    [array]$RoleMembers = Get-MgDirectoryRoleMember -DirectoryRoleId $Role.Id
+        If ($RoleMembers) {
+            Write-Host ("Members of the {0} directory role" -f $Role.displayName)
+            $RoleMembers.additionalProperties.displayName
+        }
+}
