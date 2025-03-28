6d613943cb256e78b163704f3e13655acf7c4caf
pnthr35 <pnthr35@gmail.com>
2025-03-27 22:08:31 -0700

    Adjust Intune Script

--- mgintune.ps1
+++ mgintune.ps1
@@ -4,4 +4,4 @@ Connect-MgGraph "DeviceManagementManagedDevices.Read.All"
 # -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
 
 $devices = Get-MgDeviceManagementManagedDevice -all:$true
-Write-Output $devices
+Write-Host $devices.ComplianceState
