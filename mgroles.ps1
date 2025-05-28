624362982ea655bc0b0042d51a04422c52661e5c
pnthr35 <pnthr35@gmail.com>
2025-05-27 22:04:03 -0700

    Adjust role script

--- mgroles.ps1
+++ mgroles.ps1
@@ -1,4 +1,4 @@
-# Role information
+# Get and display a Role report
 Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.ReadWrite.All"
 
 [array]$Roles = Get-MgDirectoryRole | Sort-Object displayName
@@ -6,7 +6,7 @@ Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.
 ForEach ($Role in $Roles) {
     [array]$RoleMembers = Get-MgDirectoryRoleMember -DirectoryRoleId $Role.Id
         If ($RoleMembers) {
-            Write-Host ("Members of the {0} directory role" -f $Role.displayName)
+            Write-Host ("Members of the {0} directory role:" -f $Role.displayName)
             $RoleMembers.additionalProperties.displayName
         }
 }
