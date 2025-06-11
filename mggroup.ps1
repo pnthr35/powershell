ffd0c7b4e73ba3592120d913ebc70c2c0596071e
pnthr35 <pnthr35@gmail.com>
2025-06-11 00:58:46 -0700

    Adjust role script

--- mggroup.ps1
+++ mggroup.ps1
@@ -17,9 +17,11 @@ if ($groupExists) {
     Write-Host "Group exists, deleting the Group!"
     $group = Get-MgGroup -Filter "DisplayName eq 'GroupScriptTestGraph'"
     Write-Host $group.Id
+
     # In case multiple Id's exist, you can use an index? Check on this
     # If there is only one Group though, this will fail since no array, a check on the data type maybe?
     Remove-MgGroup -GroupId $group.Id
+
 } else {
     # If group does not exist, we will create it
     Write-Host "Group does not exist, creating the group!"
