3d35359384014b9092caeb94a7111628309187a9
pnthr35 <pnthr35@gmail.com>
2025-06-14 02:00:43 -0700

    Adjust role script

--- mggroup.ps1
+++ mggroup.ps1
@@ -1,4 +1,3 @@
-# Testing Group interactions with Graph Powershell
 # Connect-MgGraph -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
 Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.ReadWrite.All"
 
