2cb247337228d7dc51fb45829f0599d3c2b756d5
pnthr35 <pnthr35@gmail.com>
2025-07-23 18:00:35 -0700

    Adjust Intune Script

--- mgintune.ps1
+++ mgintune.ps1
@@ -9,5 +9,6 @@ $devices = Get-MgDeviceManagementManagedDevice -all:$true
 foreach ($device in $devices)
 {
     # Get Device information, the most important information is detailed here
+    # TODO: Organize and possibly export to a file?
     Write-Host $device.DeviceName $device.Id $device.ComplianceState $device.DeviceEnrollmentType $device.DeviceRegistrationState $device.UserDisplayName
 }
