64a8214bd7609e9b7920a07db525ff02a7a9ee72
pnthr35 <pnthr35@gmail.com>
2025-08-01 16:07:03 -0700

    Adjust Intune Script

--- mgintune.ps1
+++ mgintune.ps1
@@ -9,11 +9,16 @@ $devices = Get-MgDeviceManagementManagedDevice -all:$true
 foreach ($device in $devices)
 {
     # Get Device information, the most important information is detailed here
-    # TODO: Organize and possibly export to a file?
+    # TODO: Organize information and write to CSV? Possibly SQL for display on internal website?
     Write-Host "Device Name:" $device.DeviceName 
     Write-Host "Device ID:" $device.Id
     Write-Host "Compliance State:" $device.ComplianceState 
     Write-Host "Enrollment Type:" $device.DeviceEnrollmentType
     Write-Host "Device Registration State:" $device.DeviceRegistrationState
     Write-Host "User Display Name:" $device.UserDisplayName
+    Write-Host "Operating System and Version:" $device.OperatingSystem $device.OSVersion
+    Write-Host "User Email:" $device.EmailAddress
+    Write-Host "Enroll Date:" $device.EnrolledDateTime
+    Write-Host "Last Sync" $device.LastSyncDateTime
+    Write-Host ""
 }
