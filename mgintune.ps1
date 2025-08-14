f28b76f7aab2e9b86bf022c871a9517b9f34bf58
pnthr35 <pnthr35@gmail.com>
2025-08-14 16:37:56 -0700

    Adjust Intune Script

--- mgintune.ps1
+++ mgintune.ps1
@@ -4,12 +4,22 @@ Connect-MgGraph "DeviceManagementManagedDevices.Read.All"
 # Use an access token if needed, generally just use delegated access for labbing
 # -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
 
+$complianceCheckReport = [System.Collections.Generic.List[Object]]::new()
 $devices = Get-MgDeviceManagementManagedDevice -all:$true
 
 foreach ($device in $devices)
 {
+    $reportItem = [PSCustomObject]@{
+        User = $device.UserDisplayName
+        deviceName = $device.DeviceName
+        complianceState = $device.ComplianceState
+    }
+
+    $complianceCheckReport.Add($reportItem)
+
     # Get Device information, the most important information is detailed here
     # TODO: Organize information and write to CSV? Possibly SQL for display on internal website?
+    <#
     Write-Host "Device Name:" $device.DeviceName 
     Write-Host "Device ID:" $device.Id
     Write-Host "Compliance State:" $device.ComplianceState 
@@ -21,4 +31,7 @@ foreach ($device in $devices)
     Write-Host "Enroll Date:" $device.EnrolledDateTime
     Write-Host "Last Sync" $device.LastSyncDateTime
     Write-Host ""
+    #>
 }
+
+$complianceCheckReport | Format-Table User, deviceName, complianceState
