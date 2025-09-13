37bfb6b8a60c2496a9ab9256ac02fe58a9f678b7
pnthr35 <pnthr35@gmail.com>
2025-09-13 03:49:05 -0700

    Adjust MgUser Script

--- mguser.ps1
+++ mguser.ps1
@@ -1,4 +1,4 @@
-# Testing User interactions in Graph Powershell
+# Getting last sign in time
 
 # This connection should run with delegated access
 Connect-MgGraph
@@ -6,18 +6,13 @@ Connect-MgGraph
 # Use with MgGraph and grab a token if needed on the current system
 # -AccessToken ($Env:token | ConvertTo-SecureString -AsPlainText -Force)
 
-# Get Users
-$UserLookup = @{}
-[array]$Users = Get-MgUser -All -Filter "usertype eq 'Member'" -Property Id, DisplayName
-
-foreach ($User in $Users) {
-    $UserLookup.Add($User.Id, $User.DisplayName)
-}
+$Users = Get-MgUser -Property Id, DisplayName, SignInActivity
 
 # Output Users
 Write-Host $UserLookup
 foreach ($User in $Users) {
-    # Find better way to format (todo)
-    $User.DisplayName + "`t`t`t" + $User.Department
+    # Find better way to format (Todo)
+    # Implement a conditional to exclude some Users like the Sync Accounts
+    $User | Format-Table Id, DisplayName, @{label="Last Sign In";Expression={$User.SignInActivity.LastNonInteractiveSignInDateTime}}
 }
 Write-Host "Done!"
