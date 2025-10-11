d59ee289aa40af8369542a4d29bfe5be94aa88aa
pnthr35 <pnthr35@gmail.com>
2025-10-11 04:25:06 -0700

    Initial Activity Script Complete

--- Get-OcyActivity.ps1
+++ Get-OcyActivity.ps1
@@ -1,18 +1,31 @@
 # TODO: Get quick sign in activities of specified User or Users...
 
-	 function Get-GetOcyActivity {
-	    [CmdletBinding()]
-	    Param(
-	     
-	    )
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory = $true,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true,
+		HelpMessage = "Enter one or multiple UPN's")]	
+	[String[]]$Users
+)
 
-	    Begin{
-            Connect-MgGraph -Scopes "AuditLog.Read.All"
-        }
+Begin {
+	Connect-MgGraph
+} # End Begin
 
-	    Process {
-	      
-	    } #End Process
+Process {
+	$userList = New-Object System.Collections.Generic.List[PSObject]
+	
+	foreach ($user in $Users) {
+		$temp = Get-MgUser -Filter "userPrincipalname eq '$user'" -Property DisplayName, SignInActivity
 
-	    End{}
-	 } #End Function
+		$userList.Add([PSCustomObject]@{
+				DisplayName = $temp.DisplayName
+				LastSignIn  = $temp.SignInActivity.LastNonInteractiveSignInDateTime
+		})
+	}
+} # End Process
+
+End {
+	$userList | Format-Table *
+}
