e752742f97ab2c09c8c38517e7ae19d3fc989d01
pnthr35 <pnthr35@gmail.com>
2026-01-18 11:13:14 -0700

    Adjust

--- Get-OcyMailbox.ps1
+++ Get-OcyMailbox.ps1
@@ -1,1 +1,10 @@
 # TODO: Get Mailbox Stats
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory = $true,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true,
+		HelpMessage = "Enter one or multiple UPN's")]	
+	[String[]]$Users
+)
+
