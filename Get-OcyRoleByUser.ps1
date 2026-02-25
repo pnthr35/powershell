e2b21c3afe004de6f1271e10a4a403237770d152
pnthr35 <pnthr35@gmail.com>
2026-02-24 17:40:50 -0700

    Adjust role scripts

--- Get-OcyRoleByUser.ps1
+++ Get-OcyRoleByUser.ps1
@@ -1,2 +1,12 @@
 # Quickly get users for a specific Role
+
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory = $true,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true,
+		HelpMessage = "Enter one or multiple UPN's")]	
+	[String[]]$Users
+)
+
 Connect-MgGraph -Scopes "Directory.Read.All", "User.Read.All", "Group.Read.All"
