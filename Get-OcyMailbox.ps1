36f8dd85424dc153d135c3384ad06aa2acdc365d
pnthr35 <pnthr35@gmail.com>
2026-03-12 16:49:40 -0700

    Adjust role scripts

--- Get-OcyMailbox.ps1
+++ Get-OcyMailbox.ps1
@@ -8,3 +8,24 @@ Param(
 	[String[]]$Users
 )
 
+Begin {
+	Connect-MgGraph -Scopes "Directory.Read.All", "User.Read.All", "Group.Read.All"
+} # End Begin
+
+Process {
+	$userList = New-Object System.Collections.Generic.List[PSObject]
+
+	foreach ($user in $Users) {
+		# Look up quick Mailbox Stats
+		# TODO
+		
+		$userList.Add([PSCustomObject]@{
+				DisplayName = $temp.DisplayName
+				LastSignIn  = $temp.SignInActivity.LastNonInteractiveSignInDateTime
+		})
+	}
+} # End Process
+
+End {
+	$userList | Format-Table *
+}
